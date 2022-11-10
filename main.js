5e6a452f3cde87961e00934d3665892c675a7ead
Tyler Hoffman <tylerhoffman@Tylers-MacBook-Pro-2.local>
2022-11-10 14:17:36 -0800

    Age Interval complete

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ const tamagotchi = {
     boredom: 0,
     age: 1,
 };
+
+let currentAge = tamagotchi.age;
+
+const ageInterval = setInterval(() => {
+    currentAge += 1;
+
+    ageBox = document.getElementById('age');
+    ageBox.textContent = currentAge;
+}, 60000);
