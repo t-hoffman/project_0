1fb458e5f353f5437c519918aad3c8279f005e37
Tyler Hoffman <tylerhoffman@Tylers-MacBook-Pro-2.local>
2022-11-10 15:28:15 -0800

    Added what pet died of, different styling

--- main.js
+++ main.js
@@ -18,9 +18,11 @@ const ageInterval = setInterval(() => {
 // Custom setInterval function to call hunger, boredom, etc.
 
 function customInterval(type, int) {
+    // Grab elements from the DOM: button/text area
     const button = document.getElementById(`${type}-btn`);
     const text = document.getElementById(`${type}-text`);
 
+    // Add button click functionality
     button.addEventListener('click', () => {
         if (tamagotchi[type] >= 0) {
             tamagotchi[type] -= 1;
@@ -30,20 +32,24 @@ function customInterval(type, int) {
         img.src = `images/${type}.png`;
     });
 
+    // Setting a time interval for the type selected
     const interval = setInterval(() =>{
+        // Game scores cannot go above 10 otherwise game is over
         if (tamagotchi[type] >= 9) {  
+            // Game over changes to the DOM, stop interval
             const body = document.querySelector('body');
             console.log(body);
             body.style.backgroundColor = '#000000';
             const img = document.querySelector('.pet-image img');
             img.src = 'images/sad.png';
+            const scores = document.querySelector('.game-scores');
+            scores.style.display = 'none';
             const buttons = document.querySelector('.buttons');
             buttons.style.display = 'none';
             const message = document.getElementById('message');
             message.style.visibility = 'visible';
-            message.textContent = 'GAME OVER!'
-            const scores = document.querySelector('.game-scores');
-            scores.style.display = 'none';
+            message.innerHTML = `GAME OVER!<br />Lilo died of ${type}`;
+        
             clearInterval(interval);
         }
 
