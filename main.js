0813a3e009344041e687325c02ae71767f7cda0d
Tyler Hoffman <tylerhoffman@Tylers-MacBook-Pro-2.local>
2022-11-13 13:45:41 -0800

    Added name function and try again button to tamagotchi

--- main.js
+++ main.js
@@ -16,18 +16,18 @@ const ageInterval = setInterval(() => {
 }, 3000);
 
 // DOM insert name
-// const input = document.getElementById('pet-input');
-// const petName = document.getElementById('pet-name');
-// input.addEventListener('input', () => {
-//     tamagotchi.name = input.value;
-//     petName.textContent = tamagotchi.name;
-// });
+const input = document.getElementById('pet-input');
 const petName = document.getElementById('pet-name');
-petName.textContent = tamagotchi.name;
+input.addEventListener('input', () => {
+    if (input.value) tamagotchi.name = input.value;
+    petName.textContent = tamagotchi.name;
+    console.log(tamagotchi.name)
+});
+
 
 // Custom setInterval function to call hunger, boredom, etc.
-
-function customInterval(type, int) {
+let intervals = {};
+function customInterval(type, int, id) {
     // Grab elements from the DOM: button/text area
     const button = document.getElementById(`${type}-btn`);
     const text = document.getElementById(`${type}-text`);
@@ -43,24 +43,36 @@ function customInterval(type, int) {
     });
 
     // Setting a time interval for the type selected
-    const interval = setInterval(() =>{
+    
+    intervals[id] = setInterval(() =>{
         // Game scores cannot go above 10 otherwise game is over
         if (tamagotchi[type] >= 9) {  
             // Game over changes to the DOM, stop interval
             const body = document.querySelector('body');
-            console.log(body);
-            body.style.backgroundColor = '#000000';
-            const img = document.querySelector('.pet-image img');
-            img.src = 'images/sad.png';
-            const scores = document.querySelector('.game-scores');
-            scores.style.display = 'none';
-            const buttons = document.querySelector('.buttons');
-            buttons.style.display = 'none';
-            const message = document.getElementById('message');
-            message.style.visibility = 'visible';
-            message.innerHTML = `GAME OVER!<br />${tamagotchi.name} died of ${type}`;
-        
-            clearInterval(interval);
+            if (body.style.backgroundColor !== '#000000') {
+                body.style.backgroundColor = '#000000';
+                const img = document.querySelector('.pet-image img');
+                img.src = 'images/sad.png';
+                const scores = document.querySelector('.game-scores');
+                scores.style.display = 'none';
+                const buttons = document.querySelector('.buttons');
+                buttons.style.display = 'none';
+                const message = document.getElementById('message');
+                message.style.visibility = 'visible';
+                message.innerHTML = `GAME OVER!<br />${tamagotchi.name} died of ${type}`;
+                if (!document.querySelector('.try-again')) {
+                    const gameArea = document.querySelector('.game-area');
+                    const newDiv = document.createElement('div');
+                    newDiv.textContent = 'Try Again';
+                    newDiv.className = 'try-again';
+                    gameArea.appendChild(newDiv);
+                    newDiv.addEventListener('click', () => {window.location.reload()});
+                }
+            }
+            
+            for (i = 1; i <= Object.keys(intervals).length; i++) {
+                clearInterval(intervals[i]);
+            }
         }
 
         tamagotchi[type] += 1;
@@ -70,6 +82,6 @@ function customInterval(type, int) {
 
 // Call customInterval for each button we have, set time
 
-customInterval('hunger', 4000);
-customInterval('sleepiness', 5000);
-customInterval('boredom', 2000);
+customInterval('hunger', 4000, 1);
+customInterval('sleepiness', 5000, 2);
+customInterval('boredom', 2000, 3);
